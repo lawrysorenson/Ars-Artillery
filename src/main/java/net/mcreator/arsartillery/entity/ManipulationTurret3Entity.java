1830c53d29b4c3ae3134b4c9ea89ba5b4006332e
Lawry Sorenson <lawrysorenson@gmail.com>
2025-02-15 21:25:38 -0700

    Flush out Man Turret 3

--- src/main/java/net/mcreator/arsartillery/entity/ManipulationTurret3Entity.java
+++ src/main/java/net/mcreator/arsartillery/entity/ManipulationTurret3Entity.java
@@ -16,21 +16,25 @@ import net.minecraftforge.network.NetworkHooks;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.MobType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.util.Mth;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
@@ -46,9 +50,12 @@ import com.hollingsworth.arsnouveau.api.util.SourceUtil;
 import net.mcreator.arsartillery.init.ArsArtilleryModItems;
 import net.mcreator.arsartillery.init.ArsArtilleryModEntities;
 
-import java.util.List;
+import javax.annotation.Nullable;
 
-public class ManipulationTurret3Entity extends Animal implements GeoEntity {
+import java.util.List;
+import java.util.EnumSet;
+
+public class ManipulationTurret3Entity extends Animal implements RangedAttackMob, GeoEntity {
 	public static final EntityDataAccessor<Boolean> SHOOT = SynchedEntityData.defineId(ManipulationTurret3Entity.class, EntityDataSerializers.BOOLEAN);
 	public static final EntityDataAccessor<String> ANIMATION = SynchedEntityData.defineId(ManipulationTurret3Entity.class, EntityDataSerializers.STRING);
 	public static final EntityDataAccessor<String> TEXTURE = SynchedEntityData.defineId(ManipulationTurret3Entity.class, EntityDataSerializers.STRING);
@@ -96,6 +103,100 @@ public class ManipulationTurret3Entity extends Animal implements GeoEntity {
 		super.registerGoals();
 		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal(this, Monster.class, true, false));
 		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
+		this.goalSelector.addGoal(1, new ManipulationTurret3Entity.RangedAttackGoal(this, 1.25, 10, 24f) {
+			@Override
+			public boolean canContinueToUse() {
+				return this.canUse();
+			}
+		});
+	}
+	
+	public class RangedAttackGoal extends Goal {
+		private final Mob mob;
+		private final RangedAttackMob rangedAttackMob;
+		@Nullable
+		private LivingEntity target;
+		private int attackTime = -1;
+		private final double speedModifier;
+		private int seeTime;
+		private final int attackIntervalMin;
+		private final int attackIntervalMax;
+		private final float attackRadius;
+		private final float attackRadiusSqr;
+
+		public RangedAttackGoal(RangedAttackMob p_25768_, double p_25769_, int p_25770_, float p_25771_) {
+			this(p_25768_, p_25769_, p_25770_, p_25770_, p_25771_);
+		}
+
+		public RangedAttackGoal(RangedAttackMob p_25773_, double p_25774_, int p_25775_, int p_25776_, float p_25777_) {
+			if (!(p_25773_ instanceof LivingEntity)) {
+				throw new IllegalArgumentException("ArrowAttackGoal requires Mob implements RangedAttackMob");
+			} else {
+				this.rangedAttackMob = p_25773_;
+				this.mob = (Mob) p_25773_;
+				this.speedModifier = p_25774_;
+				this.attackIntervalMin = p_25775_;
+				this.attackIntervalMax = p_25776_;
+				this.attackRadius = p_25777_;
+				this.attackRadiusSqr = p_25777_ * p_25777_;
+				this.setFlags(EnumSet.of(Goal.Flag.MOVE, Goal.Flag.LOOK));
+			}
+		}
+
+		public boolean canUse() {
+			LivingEntity livingentity = this.mob.getTarget();
+			if (livingentity != null && livingentity.isAlive()) {
+				this.target = livingentity;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public boolean canContinueToUse() {
+			return this.canUse() || this.target.isAlive() && !this.mob.getNavigation().isDone();
+		}
+
+		public void stop() {
+			this.target = null;
+			this.seeTime = 0;
+			this.attackTime = -1;
+			((ManipulationTurret3Entity) rangedAttackMob).entityData.set(SHOOT, false);
+		}
+
+		public boolean requiresUpdateEveryTick() {
+			return true;
+		}
+
+		public void tick() {
+			double d0 = this.mob.distanceToSqr(this.target.getX(), this.target.getY(), this.target.getZ());
+			boolean flag = this.mob.getSensing().hasLineOfSight(this.target);
+			if (flag) {
+				++this.seeTime;
+			} else {
+				this.seeTime = 0;
+			}
+			if (!(d0 > (double) this.attackRadiusSqr) && this.seeTime >= 5) {
+				this.mob.getNavigation().stop();
+			} else {
+				this.mob.getNavigation().moveTo(this.target, this.speedModifier);
+			}
+			this.mob.getLookControl().setLookAt(this.target, 30.0F, 30.0F);
+			if (--this.attackTime == 0) {
+				if (!flag) {
+					((ManipulationTurret3Entity) rangedAttackMob).entityData.set(SHOOT, false);
+					return;
+				}
+				((ManipulationTurret3Entity) rangedAttackMob).entityData.set(SHOOT, true);
+				float f = (float) Math.sqrt(d0) / this.attackRadius;
+				float f1 = Mth.clamp(f, 0.1F, 1.0F);
+				this.rangedAttackMob.performRangedAttack(this.target, f1);
+				this.attackTime = Mth.floor(f * (float) (this.attackIntervalMax - this.attackIntervalMin) + (float) this.attackIntervalMin);
+			} else if (this.attackTime < 0) {
+				this.attackTime = Mth.floor(Mth.lerp(Math.sqrt(d0) / (double) this.attackRadius, (double) this.attackIntervalMin, (double) this.attackIntervalMax));
+			} else
+				((ManipulationTurret3Entity) rangedAttackMob).entityData.set(SHOOT, false);
+		}
 	}
 
 	@Override
@@ -188,7 +289,7 @@ public class ManipulationTurret3Entity extends Animal implements GeoEntity {
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
 		builder = builder.add(Attributes.MOVEMENT_SPEED, 0);
-		builder = builder.add(Attributes.MAX_HEALTH, 20);
+		builder = builder.add(Attributes.MAX_HEALTH, 40);
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 24);
